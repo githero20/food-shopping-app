2dba9e7bf989008e519a74aaf98a35bd3af6d8b8
Omogbai Atakpu <oatakpu@boi.ng>
2023-04-18 01:35:43 +0100

    add refs to shopping list

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent {
+  @ViewChild('nameInput', {
+    static: false,
+  })
+  nameInputRef: ElementRef;
+  @ViewChild('amountInput', {
+    static: false,
+  })
+  amountInputRef: ElementRef;
 
+  onAddItem() {}
 }
